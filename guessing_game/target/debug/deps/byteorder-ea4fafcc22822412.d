6539e5fdb5e7f9421a07e3c0a43905490ae992ee
Filippo Michelini <filippomichelini@gmail.com>
2024-11-10 11:45:00 +0100

    fix project modules

---
+++ guessing_game/target/debug/deps/byteorder-ea4fafcc22822412.d
@@ -0,0 +1,7 @@
+C:\Users\fmichelini\RustroverProjects\guessing_game\target\debug\deps\libbyteorder-ea4fafcc22822412.rmeta: C:\Users\fmichelini\.cargo\registry\src\index.crates.io-6f17d22bba15001f\byteorder-1.5.0\src\lib.rs
+
+C:\Users\fmichelini\RustroverProjects\guessing_game\target\debug\deps\libbyteorder-ea4fafcc22822412.rlib: C:\Users\fmichelini\.cargo\registry\src\index.crates.io-6f17d22bba15001f\byteorder-1.5.0\src\lib.rs
+
+C:\Users\fmichelini\RustroverProjects\guessing_game\target\debug\deps\byteorder-ea4fafcc22822412.d: C:\Users\fmichelini\.cargo\registry\src\index.crates.io-6f17d22bba15001f\byteorder-1.5.0\src\lib.rs
+
+C:\Users\fmichelini\.cargo\registry\src\index.crates.io-6f17d22bba15001f\byteorder-1.5.0\src\lib.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+C:\Users\fmichelini\RustroverProjects\guessing_game\target\debug\guessing_game.exe: C:\Users\fmichelini\RustroverProjects\guessing_game\src\main.rs
