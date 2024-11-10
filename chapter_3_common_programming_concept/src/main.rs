18b80f389b6b8e508bbf84a90a8f2a17526ee641
Filippo Michelini <filippomichelini@gmail.com>
2024-11-10 17:13:37 +0100

    chapter 3 variables,functions,comments and control flow---need to understand ownership

--- chapter_3_common_programming_concept/src/main.rs
+++ chapter_3_common_programming_concept/src/main.rs
@@ -1,8 +1,16 @@
+mod control_flow;
+
 fn main() {
 
-    shadowing();
-    mutable_constants();
-    tuples();
+    // shadowing();
+    // mutable_constants();
+    // tuples();
+    // another_function(666);
+    // expression();
+    // let six:i32 = plus_one(5);//similar to expression
+    // println!("The value of 5+1 is: {six}");
+    control_flow();
+
 }
 const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
 
@@ -50,25 +58,25 @@ fn data_types(){
     let product = 4 * 30; //i32
 
     // division
-    let quotient = 56.7 / 32.2; //f64
-    let truncated = -5 / 3; // Results in -1
+    let _quotient = 56.7 / 32.2; //f64
+    let _truncated = -5 / 3; // Results in -1
 
     // remainder
-    let remainder = 43 % 5; //i32
+    let _remainder = 43 % 5; //i32
 }
 fn tuples(){
     let tup = (500, 6.4, 1);
 
-    let (x, y, z) = tup;
+    let (x, y, _z) = tup;
 
     println!("The value of y is: {y}");
     let x: (i32, f64, u8) = (500, 6.4, 1);
 
     let five_hundred = x.0;
 
-    let six_point_four = x.1;
+    let _six_point_four = x.1;
 
-    let one = x.2;
+    let _one = x.2;
 
     println!("the value of five_hundred is: {five_hundred}");
     let five_hundred = 499;
@@ -76,8 +84,22 @@ fn tuples(){
     let x=(five_hundred,7.2,2);
     let six_point_four = x.1;
     println!("the value of six_point_four is: {six_point_four}");
-
-
-
-
 }
+
+fn another_function(x: i32) {
+    println!("The value of x is: {x}");
+}
+
+fn expression(){
+    let y = {
+        let x = 3;
+        x + 1
+    }; //assign the result of an expression to a variable
+
+    println!("The value of y is: {y}");
+}
+
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+} // returns the variable plus one. Adding a semicolon would break the function because  it would leave no return variable(specified after the ->)
